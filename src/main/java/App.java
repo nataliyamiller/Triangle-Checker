76c9f35ea93cf897fe89ef9f11241badacdbb1a4
perry eising & nataliya miller <student@epicodus.com>
2015-08-17 15:57:43 -0700

    Add functionality to prohibit blank username entries.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,12 +12,17 @@ public class App {
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("username", request.session().attribute("username"));
+      
+      //Username prompt message
+      String inValidUser= "Hi. Please set your username below.";
+      model.put("inValidUser", inValidUser);
 
       model.put("template", "templates/welcome.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/triangle", (request, response) -> {
+      //show a "you need to set a username" message if the user tries to access the triangle page directly
       HashMap<String, Object> model = new HashMap<String, Object>();
 
       String inValidUser= "You need a username first!";
@@ -31,6 +36,7 @@ public class App {
     post("/triangle", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
 
+      //set up variables for the method that are received from form inputs
       int length = Integer.parseInt(request.queryParams("length"));
       int width = Integer.parseInt(request.queryParams("width"));
       int height = Integer.parseInt(request.queryParams("height"));
@@ -38,10 +44,11 @@ public class App {
       Triangle myTriangle = new Triangle(length, width, height);
       model.put("myTriangle", myTriangle);
 
+      // get the string that has been set as the username and stored in session
       String inputtedUsername = request.session().attribute("username");
 
+      //add the username key/value pair to the model
       model.put("username", inputtedUsername);
-
       model.put("template", "templates/triangle.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -50,15 +57,24 @@ public class App {
     post("/welcome", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
 
+      // get the username from the form.
       String inputtedUsername = request.queryParams("username");
-      request.session().attribute("username", inputtedUsername);
-      model.put("username", inputtedUsername);
 
+      //if username is not null or consists of whitespace let the user proceed
+      if (inputtedUsername != null && inputtedUsername.trim().length() > 0){
+        request.session().attribute("username", inputtedUsername);
+        model.put("username", inputtedUsername);
+      }
+
+      else{ //let the user know they need to set a username.
+        String inValidUser= "You need a username first!";
+        model.put("inValidUser", inValidUser);
+
+      }
+      //output
       model.put("template", "templates/welcome.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
-
 
   }
 }
